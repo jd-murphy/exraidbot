5c54579920a42a93113b0f3107c338be8d21b9a8
Jordan Murphy <jordan@blueberri-saas.com>
2018-07-14 13:40:31 -0500

    cleaned up comments

--- pyrebase_worker.py
+++ pyrebase_worker.py
@@ -16,27 +16,10 @@ config = {
 
 def push(name, phone, servers):    
     print("connecting")
-
-
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()
-    
-
-
-    # cred = credentials.Certificate("/twilio-bot-1601d-firebase-adminsdk-5bbb8-25331d6181.json")
-    # firebase = firebase_admin.initialize_app(cred)
-
-    # firebase = pyrebase.initialize_app(config)
-    # auth = firebase.auth()
-    # email = "murphyjd541@gmail.com"
-    # password = "twilioBotPyrebase666"
-    # user = auth.sign_in_with_email_and_password(email, password)   #    murphyjd541@gmail.com      twilioBotPyrebase666
-    # db = firebase.database()
-    # name = "Vinnie"
-    # phone = "333-1231-2222"
     if servers.lower() == "all":
         servers = ["bot-test-jordan", "BCS Pokemon Go", "Team Aqua's Hideout"]
-
     data = {
             name : {
                 "phone": phone,
@@ -44,8 +27,6 @@ def push(name, phone, servers):
             }
     }
     print("pushing...")
-    # Pass the user's idToken to the push method
-    # results = db.child("users").child("Bobert").set(data, user['idToken'])
     results = db.child("users").push(data)
     print('finished!\nresults ->')
     print(results)
@@ -54,20 +35,8 @@ def push(name, phone, servers):
 
 def getData():
     print("connecting")
-
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()
-    # db.child("users").get()
-
-    # cred = credentials.Certificate("/twilio-bot-1601d-firebase-adminsdk-5bbb8-25331d6181.json")
-    # firebase = firebase_admin.initialize_app(cred)
-
-    # firebase = pyrebase.initialize_app(config)
-    # auth = firebase.auth()
-    # email = "murphyjd541@gmail.com"
-    # password = "twilioBotPyrebase666"
-    # user = auth.sign_in_with_email_and_password(email, password)   #    murphyjd541@gmail.com      twilioBotPyrebase666
-    # db = firebase.database()
     print("retrieving")
     all_users = db.child("users").get()
     print('finished!\nall_users ->')
