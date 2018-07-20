4cbc1993b0af654a8564a4761f5c4f15c07e048c
Jordan Murphy <jordan@blueberri-saas.com>
2018-07-20 09:23:52 -0500

    proxy server

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
     res.send("This is my node server! Whoop!")
 });
 
-app.listen(process.env.NODE_PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log("node.js -> Listening on port 3000")
 });
+
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://127.0.0.1'}).listen(8000); // See (†)
+
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9000);
