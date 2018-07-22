63c55d5c23ea70adf216e47e94e9417b694dfcea
Jordan Murphy <jordan@blueberri-saas.com>
2018-07-22 16:04:56 -0500

    changed loop interval

--- bot.py
+++ bot.py
@@ -1037,8 +1037,9 @@ async def getServerLink(context):
 async def updateEmitInterval(context, num):
     emitInterval = int(num)
     print("emit interval updated: " + str(emitInterval) + ' seconds')
-
-
+    client.loop.stop(emit)
+    client.loop.create_task(emit())
+    await client.send_message(context.message.author, "emit interval updated: " + str(emitInterval) + ' seconds')
 
 
 
